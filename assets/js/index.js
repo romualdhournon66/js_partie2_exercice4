5d08877dded5b76a287758cbdc79fdc49fa98d91
romualdhournon66 <k2001@cegetel.net>
2018-10-09 09:22:54 +0200

    mon assets

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+function efface()
+  {
+    document.getElementById("lastname").value="";
+    document.getElementById("firstname").value="";
+    document.getElementById("city").value="";
+return false;
+}
